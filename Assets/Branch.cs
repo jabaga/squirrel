47b9afdc554c02afb5bcf41ed719a9b76d9daef9
Yasen Bagalev <evridii.badilov@gmail.com>
2019-01-26 00:45:02 +0200

    work on player

---
+++ Assets/Branch.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Branch : MonoBehaviour
+{
+    public bool isTemporary = false;
+    public bool isSwitchable = false;
+    public float temporaryTime = 1f;
+    public float switchTime = 3f;
+
+    float time = 0;
+    float timePlayerLanded = -1;
+    float timeSwitched = 0;
+    bool isDetached = false;
+    Rigidbody2D body;
+    new Collider2D collider;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+    
+    void Update()
+    {
+        if (isDetached == true)
+            return;
+
+        time += Time.deltaTime;
+
+        if(isTemporary)
+        {
+            if (time - timePlayerLanded >= temporaryTime)
+            {
+                Detach();
+            }
+        }
+
+        if(isSwitchable)
+        {
+            if(time - timeSwitched >= switchTime)
+            {
+                switchTime = time;
+                gameObject.SetActive(!gameObject.activeSelf);
+            }
+        }
+    }
+
+    void Detach()
+    {
+        isDetached = true;
+
+        body.bodyType = RigidbodyType2D.Dynamic;
+        collider.isTrigger = true;
+
+        Destroy(gameObject, 5f);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && isTemporary == true)
+        {
+            timePlayerLanded = time;
+        }
+    }
+}
---
+++ Assets/PlayerData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public static int currentLifes
+    {
+        get { return _currentLifes; }
+        set {
+            if (value > _maxLifes)
+                value = _maxLifes;
+            if (value < 0)
+                value = 0;
+
+            _currentLifes = value;
+
+            if(_currentLifes == 0)
+            {
+                // TODO die
+            }
+        }
+    }
+
+    public static int currentBullets
+    {
+        get { return _currentLifes; }
+        set
+        {
+            if (value > _maxBullets)
+                value = _maxBullets;
+            if (value < 0)
+                value = 0;
+
+            _currentLifes = value;
+        }
+    }
+
+    static int _maxLifes = 8;
+    static int _maxBullets = 50;
+    static int _currentLifes = 5;
+    static int _currentBullets = 0;
+
+}
