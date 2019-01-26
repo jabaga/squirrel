5db449aad372e5790b4f7c8aea92a29c59d211d9
Linkonko <Linkonko@abv.bg>
2019-01-26 12:07:28 +0200

    no message

---
+++ Assets/Script/Guns.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guns : MonoBehaviour
+{
+    public Transform GunPoint;
+    public Transform Bullet;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+
+            Shoot();
+        }
+    }
+
+    void Shoot() {
+        Instantiate(Bullet, GunPoint.position, GunPoint.rotation);
+    }
+}
