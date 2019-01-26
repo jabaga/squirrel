c82e2bc54bfe4d2ea400e82988956b4639a29638
vanev-coherent <iv.vanev@gmail.com>
2019-01-26 19:06:10 +0200

    Added game UI and Start and GameOver menu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : Singleton<MainMenu>
+{
+    public Button startButton, quitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(StartNewGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    void StartNewGame()
+    {
+        Application.LoadLevel("Ivan");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        
+    }
+}
