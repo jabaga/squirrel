ad26efc9c9b5372899da06295ea22979efe396e3
Yasen Bagalev <evridii.badilov@gmail.com>
2019-01-27 14:25:19 +0200

    screens

---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public float timeToLoadScene;
+    public string sceneToLoad;
+    
+    void Start()
+    {
+        Invoke("Load", timeToLoadScene);
+    }
+
+    void Load()
+    {
+        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+    }
+}
